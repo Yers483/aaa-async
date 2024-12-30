22529a856e2741671bb49c76d25e7549678d73e7
Yers <yersultan.tlembayev@gmail.com>
2024-12-31 00:04:46 +0100

    added hw

--- homework/tasks/task_6.py
+++ homework/tasks/task_6.py
@@ -18,14 +18,20 @@ class BackgroundCoroutinesWatcher:
         # Здесь необходимо реализовать логику планирования корутины.
         #
         # YOUR CODE GOES HERE
+        task = asyncio.create_task(coro)
+        self._running_tasks.add(task)
+        task.add_done_callback(self._remove_from_running_task)
 
     def _remove_from_running_task(self, task: asyncio.Task) -> None:
         self._running_tasks.remove(task)
 
     async def close(self):
-        # Здесь необходимо реализовать отмену корутин, которые ещё не успели завершиться.
+        # Здесь необходимо реализовать отмену корутин,
+        # которые ещё не успели завершиться.
         #
         # YOUR CODE GOES HERE
+        for task in self._running_tasks:
+            task.cancel()
 
 
 class FastHandlerWithLongBackgroundTask:
@@ -40,6 +46,7 @@ class FastHandlerWithLongBackgroundTask:
         self._bcw.schedule_soon(coro)
 
     async def close(self) -> None:
-        # Этот метод будет вызван при завершении работы, все незавершённые корутины
+        # Этот метод будет вызван при завершении работы,
+        # все незавершённые корутины
         # полученные из create_long_task, должны быть отменены.
         await self._bcw.close()
